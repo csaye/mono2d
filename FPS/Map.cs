0441cb282aa242172b52944933783794fd7d1f5d
Cooper Saye <coopersaye@gmail.com>
2021-04-09 15:09:56 -0600

    Implement wall drawing.

--- FPS/Map.cs
+++ FPS/Map.cs
@@ -6,6 +6,8 @@ namespace FPS
 {
     public class Map
     {
+        private bool[,] map;
+
         private Vector2 playerPosition = new Vector2(16, 16);
         private float playerAngle = 0;
         private float playerSpeed;
@@ -14,6 +16,18 @@ namespace FPS
         private const float WalkSpeed = 8;
         private const float RotationSpeed = 1.5f;
 
+        private const float Fov = (float)Math.PI / 4;
+
+        private const float RayStep = 0.1f;
+        private const float MaxDepth = Drawing.GridWidth;
+
+        public Map()
+        {
+            int width = Drawing.GridWidth;
+            int height = Drawing.GridHeight;
+            map = new bool[width, height];
+        }
+
         public void Update(GameTime gameTime, Game1 game)
         {
             ProcessKeyboardState(game);
@@ -21,6 +35,55 @@ namespace FPS
             float delta = (float)gameTime.ElapsedGameTime.TotalSeconds;
 
             MovePlayer(delta);
+        }
+
+        public void Draw(Game1 game)
+        {
+            // Cast ray for each grid
+            for (int x = 0; x < Drawing.GridWidth; x++)
+            {
+                // Calculate ray angle and starting position
+                float rayAngle = playerAngle - (Fov / 2) + (Fov * ((float)x / Drawing.GridWidth));
+                Vector2 rayPosition = playerPosition;
+                float rayDistance = 0;
+                bool hitWall = false;
+
+                // Cast ray
+                while (!hitWall && rayDistance < MaxDepth)
+                {
+                    // If out of bounds, wall hit
+                    if (rayPosition.X < 0 || rayPosition.X >= Drawing.GridWidth || rayPosition.Y < 0 || rayPosition.Y >= Drawing.GridHeight)
+                    {
+                        hitWall = true;
+                    }
+                    // If in bounds, check whether wall hit
+                    else
+                    {
+                        int mapX = (int)rayPosition.X;
+                        int mapY = (int)rayPosition.Y;
+                        if (map[mapX, mapY]) hitWall = true;
+                    }
+
+                    // If wall not hit, increment ray distance
+                    if (!hitWall)
+                    {
+                        rayPosition.X += RayStep * (float)Math.Sin(rayAngle);
+                        rayPosition.Y += RayStep * (float)Math.Cos(rayAngle);
+                        rayDistance += RayStep;
+                    }
+                }
+
+                // Get wall height and y
+                float closeFactor = (1 - (rayDistance / MaxDepth));
+                int wallHeight = (int)(Drawing.Height * closeFactor);
+                int wallY = (Drawing.Height / 2) - (wallHeight / 2);
+
+                // Draw wall rect
+                Rectangle rect = new Rectangle(x * Drawing.Grid, wallY, Drawing.Grid, wallHeight);
+                int colorFactor = (int)(255 * closeFactor);
+                Color color = new Color(colorFactor, colorFactor, colorFactor);
+                Drawing.DrawRect(rect, color, game);
+            }
         }
 
         private void ProcessKeyboardState(Game1 game)
